f8cd935300145b036bdff127d1c6b92ae046650b
AgasgG <agasgg@mail.ru>
2020-04-20 14:25:38 +0300

    modules: Orders/models.py (edit: add total_price of Order)
    style:
    button (edit)
    
    html:
    button (edit)
    
    db:
    price (model Product)
    
    media:
    --

--- Orders/models.py
+++ Orders/models.py
@@ -1,5 +1,8 @@
 # Описание полей в таблице БД
 from django.db import models
+from django.db.models import signals
+from django.db.models.signals import post_save
+
 from Products.models import Product
 
 class Status(models.Model):
@@ -18,7 +21,7 @@ class Status(models.Model):
 
 
 class Order(models.Model):
-    total_price = models.DecimalField(max_digits=7, decimal_places=2, default=0) #цена для всех товаров в заказе
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0) #цена для всех товаров в заказе
     customer_name = models.CharField(max_length=64, blank=True, null=True, default=None)  # обязательный атрибут для текстового поля это максимальная длина
     customer_email = models.EmailField(blank=True, null=True, default=None) #тип поля имэйл
     customer_phone = models.CharField(max_length=48, blank=True, null=True, default=None) #Blank - поле может быть пустым
@@ -36,13 +39,17 @@ class Order(models.Model):
         verbose_name = 'Заказ'
         verbose_name_plural = 'Заказы'
 
+    def save(self, *args, **kwargs):
+        super(Order, self).save(*args, **kwargs)
+
+
 
 class ProductInOrder(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, blank=True, null=True, default=None) #ключ для соединения с заказом
     product = models.ForeignKey(Product, on_delete=models.CASCADE, blank=True, null=True, default=None)  # ключ для соединения с заказом
     nmb = models.IntegerField(default=1) #nmb - numbers, количество товаров
-    price_per_item = models.DecimalField(max_digits=7, decimal_places=2, default=0) #цена за единицу товара. Если цена меняется, то в заказе можно будет увидеть цену продажи
-    total_price = models.DecimalField(max_digits=7, decimal_places=2, default=0) #price*nmb
+    price_per_item = models.DecimalField(max_digits=10, decimal_places=2, default=0) #цена за единицу товара. Если цена меняется, то в заказе можно будет увидеть цену продажи
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0) #price*nmb
     is_active = models.BooleanField(default=True)  # для того чтобы скрывать товар в заказе, если его отменили
     created = models.DateTimeField(auto_now_add=True, auto_now=False) #значение меняется при создании
     updated = models.DateTimeField(auto_now_add=False, auto_now=True) #значение меняется при любом обновлении в модели/записи
@@ -52,5 +59,25 @@ class ProductInOrder(models.Model):
 
     class Meta: #произносимое имя.
 # Задаётся имя в единственном и множественном числе:
-        verbose_name = 'Товар'
-        verbose_name_plural = 'Товары в заказах'
+        verbose_name = 'Товар в заказе'
+        verbose_name_plural = 'Товары в заказе'
+
+    def save(self, *args, **kwargs): #функция для переопределения сохранения
+        price_per_item = self.product.price #цена в поле Модели Продукта присваивается цене в строке заказа
+        self.price_per_item =  price_per_item # цене в строке заказа
+        self.total_price = self.nmb * price_per_item #считаем итоговую сумму
+
+        super(ProductInOrder, self).save(*args, **kwargs)
+
+def product_in_order_post_save(sender, instance, created, **kwargs):
+    order = instance.order
+    all_products_in_order = ProductInOrder.objects.filter(order=order, is_active=True)
+
+    order_total_price = 0
+    for item in all_products_in_order:
+       order_total_price += item.total_price
+
+    instance.order.total_price = order_total_price
+    instance.order.save(force_update=True)
+
+post_save.connect(product_in_order_post_save, sender=ProductInOrder)
--- Products/models.py
+++ Products/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=64, blank=True, null=True, default=None)  # обязательный атрибут для текстового поля это максимальная длина
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     description = models.TextField(blank=True, null=True, default=None)#несколько строк
     is_active = models.BooleanField(default=True)  # для того чтобы скрывать товар, допустим его нет
     created = models.DateTimeField(auto_now_add=True, auto_now=False) #значение меняется при создании
